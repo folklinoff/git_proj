b1daaf1581c8e2437b33efc575f6cf6d01f57235
Maksud Saydkhodzhaev <maksud.saidhodzhaev@mail.ru>
2023-03-08 12:31:41 +0300

    Initial commit

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+)
+
+//This function is for greeting a user
+func Greet(name string) {
+  fmt.Printf("Hello, %v\n", name)
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+)
+
+//This is the entry point for a project
+func main() {
+  Greet("Maksud")
+}
